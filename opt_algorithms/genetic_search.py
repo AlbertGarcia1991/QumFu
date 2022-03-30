54533a2ae47305f10394ec38ee1d2f5418b79070
Albert_Garcia_Plaza <albert@quell.tech>
2022-03-30 10:46:15 +0100

    First branch commit

---
+++ opt_algorithms/genetic_search.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# Differential Evolution Optimization (DEO)
+
+# Non-dominant Sorting Genetic Algorithms (NSGA-II)
---
+++ opt_algorithms/gradient_based_search.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# Epsilon-Constraint
+
+# Particle Swarm Optimization (PSO)
